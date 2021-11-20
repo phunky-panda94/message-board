f8333c4ea31d374a60888ce92d17e4d1f6fe833e
phunky-panda94 <victor.nim94@outlook.com>
2021-11-20 15:38:42 +1100

    completed basic message functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+    {
+        text: 'Hi there!',
+        user: 'John',
+        added: new Date().toLocaleDateString()
+    },
+    {
+        text: 'Hey',
+        user: 'Charlie',
+        added: new Date().toLocaleDateString()
+    }
+]
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Message Board',
+        messages: messages
+    });
 });
+
+router.post('/new', function (req, res, next) {
+    messages.push(
+        {
+            text: req.body.message,
+            user: req.body.author,
+            added: new Date().toLocaleDateString()
+        }
+    );
+    res.redirect('/');
+})
 
 module.exports = router;
